c2c782251fa93bcb8b851674a81a45f5e7aa082e
Xenia Specka <specka@zalf.de>
2018-07-11 12:38:38 +0200

    * updated climate files with et0

---
+++ phase2/step1/README.txt
@@ -0,0 +1,5 @@
+Python 2.7 (x64)
+- pandas
+- zmq
+
+MONICA Windows installer (x64) -
--- phase2/step1/monica_simulation_setup/input_data/climate/generate_monica_climate_files_from_agmip_xlsx.py
+++ phase2/step1/monica_simulation_setup/input_data/climate/generate_monica_climate_files_from_agmip_xlsx.py
@@ -9,6 +9,7 @@ excel_file = pd.ExcelFile(input_climate_file)
 # list of sheet names
 sheets = excel_file.sheet_names
 
+
 # go through all sheets
 for sheet_name in sheets:
 
@@ -21,21 +22,21 @@ for sheet_name in sheets:
     with open(csv_filename, "wb") as csv_fp:
 
         csv_out = csv.writer(csv_fp, delimiter=",")
-        csv_out.writerow(["iso-date", "tmin", "tmax", "tavg", "globrad", "precip","wind", "relhumid"])
+        csv_out.writerow(["iso-date", "tmin", "tmax", "tavg", "globrad", "precip","et0"])
 
         for df_row in sheet_df.iterrows():
             row_value = df_row[1]
+
             date = datetime.date(year=int(row_value['YYYY']), month=int(row_value['MM']), day=int(row_value['DD']))
             date_str = date.isoformat()
 
-            globrad = round(float(row_value['SRAD']), 3)
+            globrad = round(float(row_value['SRAD']), 5)
             tmin = round(float(row_value['TMIN']), 2)
             tmax = round(float(row_value['TMAX']), 2)
             tavg = round(float(tmin+tmax) / 2.0, 2)
-            wind = 0.0                          # float(row_value['WIND'])
-            relhumid = 80.0                     # float(row_value['RHUM'])
-            precip = round(float(row_value['RAIN']), 2)
+            et0 = round(float(row_value['ET0']), 5)
+            precip = round(float(row_value['RAIN']), 3)
 
-            csv_out.writerow([date, tmin, tmax, tavg, globrad, precip, wind, relhumid ])
+            csv_out.writerow([date, tmin, tmax, tavg, globrad, precip, et0])
 
         #print(date)
